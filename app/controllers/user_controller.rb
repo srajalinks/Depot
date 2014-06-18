187db51cb7e2218e365bc19c1f6c56f3f6fded73
srajalinks <srajalinks@gmail.com>
2014-06-18 12:05:07 +0530

    Initial Commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,17 @@
+class UserController < ApplicationController
+  def new
+    render :text => "Hello World!!!!"
+  end
+
+  def save
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/user_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UserControllerTest < ActionController::TestCase
+  test "should get [new" do
+    get :[new
+    assert_response :success
+  end
+
+  test "should get save" do
+    get :save
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
+
+  test "should get destroy]" do
+    get :destroy]
+    assert_response :success
+  end
+
+end
